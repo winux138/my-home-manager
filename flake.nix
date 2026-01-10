c8bb76b8ef6732d5d73d53b148d6c40bf5fb7f84
ju <thomas.julien@protonmail.com>
2026-01-10 14:00:16 +0100

    Fix compilation

--- flake.nix
+++ flake.nix
@@ -16,7 +16,7 @@
     system = "x86_64-linux";
     pkgs = import nixpkgs { inherit system; };
   in {
-    homeConfiguration = {
+    homeConfigurations = {
       ubuntu-home = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
 	modules = [ ./home.nix ];
