454197dbebb30d9cf15be7a399c608fe10739614
Julien Thomas <julien.thomas@external.roche.com>
2026-06-12 14:21:45 +0200

    Add caveman skill to opencode

--- home-office.nix
+++ home-office.nix
@@ -52,24 +52,24 @@
   programs.opencode = {
     enable = true;
     package = unstable.opencode;
-    agents = {
-      AGENTS = ''
-        Drop: articles (a/an/the), filler (just/really/basically/actually/simply), pleasantries (sure/certainly/of course/happy to), hedging. Fragments OK. Short synonyms (big not extensive, fix not "implement a solution for"). Technical terms exact. Code blocks unchanged. Errors quoted exact.
+    rules = ''
+      Drop: articles (a/an/the), filler (just/really/basically/actually/simply), pleasantries (sure/certainly/of course/happy to), hedging. Fragments OK. Short synonyms (big not extensive, fix not "implement a solution for"). Technical terms exact. Code blocks unchanged. Errors quoted exact.
 
-        Pattern: `[thing] [action] [reason]. [next step].`
+      Pattern: `[thing] [action] [reason]. [next step].`
 
-        Not: "Sure! I'd be happy to help you with that. The issue you're experiencing is likely caused by..."
-        Yes: "Bug in auth middleware. Token expiry check use `<` not `<=`. Fix:"
+      Not: "Sure! I'd be happy to help you with that. The issue you're experiencing is likely caused by..."
+      Yes: "Bug in auth middleware. Token expiry check use `<` not `<=`. Fix:"
 
-        Favor simple, short and concise changes. Don't yap, no fluff.
-      '';
-    };
+      Favor simple, short and concise changes. Don't yap, no fluff.
+    '';
   };
 
   home = {
     packages = with pkgs; [
       # proprietary / work related
       # vscode
+
+      gh
 
       meld
       plantuml
