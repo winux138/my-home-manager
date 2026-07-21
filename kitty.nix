e44ea33eb2b12b6d3d9e12aebc0b09fe623d57f1
Julien Thomas <julien.thomas@external.roche.com>
2026-07-21 16:12:25 +0200

    Add some kitty config

--- kitty.nix
+++ kitty.nix
@@ -9,6 +9,27 @@
     # Kitty configuration goes here.
     settings = {
       background_opacity = 0.95;
+
+      # Bell
+      enable_audio_bell = false;
+      visual_bell_duration = 0.0;
+
+      # Copy/paste / clipboard
+      copy_on_select = "clipboard";
+      strip_trailing_spaces = "smart";
+
+      # Links
+      detect_urls = true;
+      underline_hyperlinks = "always";
+
+      # Shell integration
+      shell_integration = "enabled";
+
+      # Performance / updates
+      sync_to_monitor = true;
+      repaint_delay = 10;
+      input_delay = 3;
+      update_check_interval = 0;
     };
   };
 }
